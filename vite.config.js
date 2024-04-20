c3b1e337a3e7967c308fbb3e29be37ecce0fc115
Ronieee18 <ronitparwani78@gmail.com>
2024-04-20 20:09:12 +0530

    committed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import copy from 'rollup-plugin-copy'; // Import rollup-plugin-copy
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    copy({
+      targets: [{ src: 'sitemap.xml', dest: 'dist' }], // Copy sitemap.xml to the output directory
+    }),
+  ],
+});
